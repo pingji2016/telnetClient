9c04e38a73fea1e1734ef2e8570b9015ac30526e
pingji2016 <3192086411@qq.com>
2021-10-16 14:28:14 +0800

    [zhaofm]项目初始化

---
+++ src/main/java/com/example/telnetclinet/TelnetClinetApplication.java
@@ -0,0 +1,13 @@
+package com.example.telnetclinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelnetClinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelnetClinetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telnetclinet/TelnetClinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telnetclinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelnetClinetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
